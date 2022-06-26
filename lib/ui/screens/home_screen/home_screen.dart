cf67c24ff479bf971713d6465b8616a101123f70
Christian <chibuike4jamb@gmail.com>
2022-06-26 23:38:33 +0100

    testing alarms...014

--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../../widgets/clock/clock_view.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -15,9 +16,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   var timeNow = DateTime.now();
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    Timer.periodic(Duration(milliseconds: 100), (timer){
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
       setState(() {
         timeNow = DateTime.now();
       });
@@ -31,41 +32,86 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     var formattedTime = DateFormat('HH : mm : ss').format(timeNow);
     var formattedDate = DateFormat('dd-MM-yyyy').format(timeNow);
+    var timeZoneString = timeNow.timeZoneOffset.toString().split('.').first;
+var offsetSign = '';
+if(!timeZoneString.startsWith('-')){
+  offsetSign = '+';
+}
+print(timeZoneString);
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16),
-        child: Container(
-          color: Theme.of(context).backgroundColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClockView(),
-              Text(formattedTime, style: appThemeLight.primaryTextTheme.headline1,),
-              Text(formattedDate, style: appThemeLight.primaryTextTheme.headline1,),
-
-              Text("O'Clock", style: appThemeLight.primaryTextTheme.headline1,),
-              SizedBox(
-                height: 20,
-              ),
-              Text("Timezone", style: appThemeLight.primaryTextTheme.headline2,),
-              Row(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.language),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text("UTC/GMT", style: appThemeLight.primaryTextTheme.headline2,),
+MaterialButton(onPressed: (){}, child: Column(
+  children: [
+    FlutterLogo(),
+          Text('World Clock', style: appThemeLight.textTheme.headline3),
+  ],
+),)
                 ],
               ),
-              Text("O'Clock"),
-              Text("O'Clock"),
-              Text("O'Clock"),
-            ],
-          ),
+            ),
+            Divider(),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20),
+                color: Theme.of(context).backgroundColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                        child: ClockView()),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      formattedTime,
+                      style: appThemeLight.primaryTextTheme.headline1,
+                    ),
+                    Text(
+                      formattedDate,
+                      style: appThemeLight.primaryTextTheme.headline1,
+                    ),
+                    Text(
+                      "O'Clock",
+                      style: appThemeLight.primaryTextTheme.headline1,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Timezone",
+                      style: appThemeLight.primaryTextTheme.headline2,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.language),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "UTC/GMT " + offsetSign + timeZoneString,
+                          style: appThemeLight.primaryTextTheme.headline2,
+                        ),
+                      ],
+                    ),
+                    Text("O'Clock"),
+                    Text("O'Clock"),
+                    Text("O'Clock"),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
